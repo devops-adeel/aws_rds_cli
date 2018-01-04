cd0708d6661851b44abb87c9343ff1cf0a9c4176
Adeel Ahmad <adeelahmad786@gmail.com>
2018-01-04 11:43:32 +0000

    got working snapshot retreival with cluster check

--- latest_snapshot.py
+++ latest_snapshot.py
@@ -18,6 +18,19 @@ click.disable_unicode_literals_warning = True
 RDS = boto3.client('rds')
 
 
+def query_db_cluster(instanceid):
+    """
+    Querying whether DB is Clustered or not
+    """
+    try:
+        db_instance = RDS.describe_db_instances(
+            DBInstanceIdentifier=instanceid
+            )
+        return db_instance['DBInstances'][0]['DBClusterIdentifier']
+    except KeyError:
+        return False
+
+
 @click.command()
 @click.option('--instanceid', envvar='DBINSTANCEID',
               help='The ID of the DB Instance.')
@@ -25,15 +38,28 @@ def cli(instanceid):
     """
     This is a cli tool to find the latest snapshot for a given RDS instanceid
     """
-    try:
-        snapshots = RDS.describe_db_snapshots(
-            DBInstanceIdentifier=instanceid
-            )
-        latest = sorted(snapshots['DBSnapshots'], key=lambda item:
-                        item['SnapshotCreateTime'], reverse=True)[0]['DBSnapshotIdentifier']
-        click.echo(latest)
-    except ClientError as error:
-        click.echo(error)
+    if query_db_cluster(instanceid):
+        clusterid = query_db_cluster(instanceid)
+        try:
+            snapshots = RDS.describe_db_cluster_snapshots(
+                DBClusterIdentifier=clusterid
+                )
+            latest = sorted(snapshots['DBClusterSnapshots'], key=lambda item:
+                            item['SnapshotCreateTime'], reverse=True) \
+                            [0]['DBClusterSnapshotIdentifier']
+            click.echo(latest)
+        except ClientError as error:
+            click.echo(error)
+    else:
+        try:
+            snapshots = RDS.describe_db_snapshots(
+                DBInstanceIdentifier=instanceid
+                )
+            latest = sorted(snapshots['DBSnapshots'], key=lambda item:
+                            item['SnapshotCreateTime'], reverse=True)[0]['DBSnapshotIdentifier']
+            click.echo(latest)
+        except ClientError as error:
+            click.echo(error)
 
 if __name__ == '__main__':
     cli()
